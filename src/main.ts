9d2d7f9a546f2775a1a74576fef164a49fc1fd10
Daniel Nuez <alu0100881165@ull.edu.es>
2021-05-07 17:46:41 +0100

    Cambio en la cors policy

--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,7 @@ async function bootstrap() {
 		allowedHeaders: '*',
 	});
 	app.use((req, res, next) => {
-		res.header('Access-Control-Allow-Origin', '*');
+		res.header('Access-Control-Allow-Origin', 'http://localhost:8100');
 		res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
 		res.header('Access-Control-Allow-Origin', 'Content-Type, Accept');
 	});
